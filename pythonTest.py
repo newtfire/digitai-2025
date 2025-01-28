d3f5d8bddee77e1b95e998f411d4da45ad7b7675
ebeshero <ebbondar@gmail.com>
2025-01-28 12:17:21 -0500

    adding model options we will pursue

--- pythonTest.py
+++ pythonTest.py
@@ -2,8 +2,12 @@ import ollama
 
 # https://www.youtube.com/watch?v=4T4Fr20yzBw
 model = 'llama3.2'
+model1b = 'llama3.2:1b'
+model3b = 'llama3.2:3b'
+model70b = 'llama3.3:70b'
 
-prompt = "what color is the sky?"
+
+prompt = "How can I learn more about how TEI handles link associations between documents?"
 
 stream = ollama.chat(
     model=model,
