ad5bc39b3b4086e7d103a75719e674a06d1bfb67
Hadleigh Jae Bills <hjb5471@psu.edu>
2025-02-04 11:31:10 -0500

    a few test files

---
+++ uhhhh.py
@@ -0,0 +1,45 @@
+# Importing the ollama module which is a chatbot API.
+import ollama
+from pathlib import Path
+from llama_index.readers.file import XMLReader
+from llama_index.core import VectorStoreIndex
+import xml.etree.ElementTree as ET
+model = 'llama3.2'
+
+# Defining a function that reads a file and returns its content.
+
+loader = XMLReader()
+file = loader.load_data(file=Path("./p5subset.xml"))
+def read_file(file):
+    # Try-except block for handling FileNotFoundError exception if the file does not exist.
+    try:
+        with open(file, 'r') as f:
+            data = f.read(file)   # Reading the file content and storing it in variable "data".
+        return data  # Returning the read data.
+    except FileNotFoundError:
+        print("The file could not be found.")
+
+# Calling the function with a filename as argument to get its content.
+data = read_file('./p5subset.xml')
+
+# print("# Input JSON content with multiple errors")  # Printing the content of the JSON file.
+# print(data)  # Printing the content of the JSON file.
+
+# Using ollama's chat method for getting a response from the model 'mistral'. The system role is set to help with coding issues, and user provides the data in JSON format that needs fixing. Temperature option is also provided.
+ollama_response = ollama.chat(model='model', messages=[
+  {
+    'role': 'system',
+    'content': 'You are a helpful coding assistant.',
+  },
+  {
+    'role': 'user',
+    'content': f'What is the title of the document',
+  },
+],
+options = {
+  #'temperature': 1.5, # very creative
+  'temperature': 0 # very conservative (good for coding and correct syntax)
+})
+
+# Printing the response from ollama.
+print(ollama_response['message']['content'])
