d3dbc263d3c88f22572f570a43ec975eecd497d2
ebeshero <ebbondar@gmail.com>
2025-02-04 12:33:09 -0500

    Improved uhhh

--- uhhhh.py
+++ uhhhh.py
@@ -9,31 +9,34 @@ model = 'llama3.2'
 # Defining a function that reads a file and returns its content.
 
 loader = XMLReader()
+
 file = loader.load_data(file=Path("./p5subset.xml"))
+
 def read_file(file):
     # Try-except block for handling FileNotFoundError exception if the file does not exist.
     try:
         with open(file, 'r') as f:
-            data = f.read(file)   # Reading the file content and storing it in variable "data".
+            data = f.read()   # Reading the file content and storing it in variable "data".
         return data  # Returning the read data.
     except FileNotFoundError:
         print("The file could not be found.")
 
 # Calling the function with a filename as argument to get its content.
-data = read_file('./p5subset.xml')
+
+# data = read_file('./p5subset.xml')
 
 # print("# Input JSON content with multiple errors")  # Printing the content of the JSON file.
 # print(data)  # Printing the content of the JSON file.
 
 # Using ollama's chat method for getting a response from the model 'mistral'. The system role is set to help with coding issues, and user provides the data in JSON format that needs fixing. Temperature option is also provided.
-ollama_response = ollama.chat(model='model', messages=[
+ollama_response = ollama.chat(model=model, messages=[
   {
     'role': 'system',
     'content': 'You are a helpful coding assistant.',
   },
   {
     'role': 'user',
-    'content': f'What is the title of the document',
+    'content': f'I need help with coding in TEI, and work',
   },
 ],
 options = {
