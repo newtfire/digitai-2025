000f1bf0f552306e9c776b6eea468b44f41a5155
Hadleigh Jae Bills <hjb5471@psu.edu>
2025-05-19 15:20:09 -0400

    testing lxml

---
+++ lxml_tests/lxml_elements.py
@@ -0,0 +1,21 @@
+# List all unique elements
+from lxml import etree
+
+dataset_file = "../teiTester-dmJournal.xml"
+tree = etree.parse(dataset_file)
+parent_tag = 'teiHeader'
+
+# Get all elements
+# elements = tree.xpath("//*")
+elements = tree.xpath(f"//*[local-name()='{parent_tag}']//*")
+
+# Use a set to collect unique tag names (namespace stripped)
+unique_tags = set()
+
+for element in elements:
+    tag = element.tag.split('}', 1)[1] if '}' in element.tag else element.tag
+    unique_tags.add(tag)
+
+# Print unique tag names
+for tag in sorted(unique_tags):
+    print(tag)
